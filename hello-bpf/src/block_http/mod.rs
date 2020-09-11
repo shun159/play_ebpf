07de749b362c838c24e3efec8ffd2945ae42174b
Eishun Kondoh <shun3382@gmail.com>
2020-09-11 12:32:46 +0900

    xdp: Add hello-bpf example

---
+++ hello-bpf/src/block_http/mod.rs
@@ -0,0 +1,9 @@
+// This is where you should define the types shared by the kernel and user
+// space, eg:
+//
+// #[repr(C)]
+// #[derive(Debug)]
+// pub struct SomeEvent {
+//     pub pid: u64,
+//     ...
+// }
